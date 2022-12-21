f7bb7420b161a8a73f6128b67950bae2873f6030
xlan11 <alankelly@gmail.com>
2022-12-21 13:07:52 +0000

    refactoring and added receipt

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const dayRate = 34;
-const halfdayRate = 20;
 const govPercent = .8;
+const fullDayHours = 8.5;
+const halfDayHours = 4;
 
 const kids = document.getElementById('numKids');
 const fullDays = document.getElementById('numDays');
 const halfDays = document.getElementById('numHalfDays');
 const displayBreakdown = document.getElementById('cost-breakdown')
+let hourlyRate = document.getElementById('hourlyPrice')
 
 
 function totalCost() {
@@ -13,13 +14,19 @@ function totalCost() {
         alert("Please enter a number for every box"); 
     }
         else {
-    let grossCost = (parseFloat(kids.value) * ((parseFloat(fullDays.value) * dayRate) + (parseFloat(halfDays.value) * halfdayRate)));
+
+    let dayRate = hourlyRate.value * fullDayHours;
+    let halfDayRate = hourlyRate.value * halfDayHours;
+
+    let grossCost =  (parseFloat(kids.value)) * (parseFloat(dayRate)) + (parseFloat(halfDayRate));
     grossCost=grossCost.toFixed(2)
 
-    let netCost = (parseFloat(kids.value) * ((parseFloat(fullDays.value) * dayRate) + (parseFloat(halfDays.value) * halfdayRate))) * govPercent;
+    let netCost =  (parseFloat(kids.value)) * (parseFloat(dayRate)) + (parseFloat(halfDayRate)) * govPercent;
     netCost = netCost.toFixed(2);
+    
     let monthly = netCost * 4;
     monthly = monthly.toFixed(2);
+    
     let govSaving = (grossCost / 100) * 20;
     govSaving = govSaving.toFixed(2);
     
@@ -31,19 +38,12 @@ function totalCost() {
         <li>Your childcare after deductions is <strong>£${netCost}</strong> per week</li>
         <li>This works out to £${monthly} per month assuming a four week month</li>
     </ul>
-    `
-    // displayGrossCost.innerHTML = `Your total childcare before deductions is costing you <strong>£${grossCost}</strong> per week`;
-    // displayNetCost.innerHTML = `Your childcare after deductions is costing you <strong>£${netCost}</strong> per week`;
-    // displayFullDays.innerHTML = `Your child(ren) are in childcare for ${fullDays.value} full days per week.`;
-    // displayHalfDays.innerHTML = `Your child(ren) are in childcare for ${halfDays.value} half days per week.`;
-    
-    // let monthly = netCost * 4;
-    // displayMonthlyCost.innerHTML = `This works out to £${monthly} per month assuming a four week month`;
-
-    // let govSaving = (grossCost / 100) * 20;
-    // govSaving = govSaving.toFixed(2);
-    // displayGov.innerHTML = `<a href="https://www.gov.uk/tax-free-childcare" target="_blank">Tax-free childcare scheme</a> is saving you £${govSaving} per week`;
-    
+    <div class="math">
+            <h2>Receipt</h2>
+            <p>Gross cost: ${grossCost}</p>
+            <p class="saving">Tax-free saving: -${govSaving}</p>
+            <hr>
+            <p><strong>Total weekly bill: ${netCost}</strong></p>
+    </div>
+    `}
     }
-} 
-
