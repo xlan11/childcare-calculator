c4815e88bb8c93e5995f17c040cda666b0f90be8
xlan11 <alankelly@gmail.com>
2022-12-21 16:42:20 +0000

    testing number kb on mobile

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ let hourlyRate = document.getElementById('hourlyPrice')
 
 
 function totalCost() {
-    if (kids.value == 0 || fullDays.value == "" || halfDays.value == ""){
+    // if (kids.value == 0 || fullDays.value == "" || halfDays.value == "")
+    if (fullDays.value == "" || halfDays.value == ""){
         alert("Please enter a number for every box"); 
     }
         else {
@@ -18,16 +19,20 @@ function totalCost() {
     let dayRate = (hourlyRate.value * fullDays.value) * fullDayHours;
     let halfDayRate = (hourlyRate.value * halfDays.value) * halfDayHours;
 
-    let grossCost =  (parseFloat(kids.value)) * (parseFloat(dayRate)) + (parseFloat(halfDayRate));
+    let grossCost = (parseFloat(dayRate)) + (parseFloat(halfDayRate));
     grossCost=grossCost.toFixed(2)
     
+    // let grossCost =  (parseFloat(kids.value)) * (parseFloat(dayRate)) + (parseFloat(halfDayRate));
+    // grossCost=grossCost.toFixed(2)
+
     let govSaving = (grossCost / 100) * 20;
     govSaving = govSaving.toFixed(2);
 
-    let netCost =  (parseFloat(kids.value)) * (parseFloat(dayRate)) + (parseFloat(halfDayRate)) - govSaving;
+    let netCost =  (parseFloat(dayRate)) + (parseFloat(halfDayRate)) - govSaving;
     netCost = netCost.toFixed(2);
-    
-    // console.log('dayrate is ' + dayRate + ' half day rate is ' + halfDayRate)
+
+    // let netCost =  (parseFloat(kids.value)) * (parseFloat(dayRate)) + (parseFloat(halfDayRate)) - govSaving;
+    // netCost = netCost.toFixed(2);
 
     let monthly = netCost * 4;
     monthly = monthly.toFixed(2);
@@ -35,7 +40,8 @@ function totalCost() {
 
     
     displayBreakdown.innerHTML =
-    `<ul>
+    `<h2>Cost breakdown:</h2>
+    <ul>
         <li>Your child(ren) are in childcare for ${fullDays.value} full days and ${halfDays.value} half days per week.</li>
         <li>Your total childcare before deductions is <strong>£${grossCost}</strong> per week</li>
         <li><a href="https://www.gov.uk/tax-free-childcare" target="_blank">Tax-free childcare scheme</a> is saving you £${govSaving} per week</li>
